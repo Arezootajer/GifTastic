377d546f17464d062967a49ce5739c6078858054
dblanchard13 <davidb.prof@gmail.com>
2017-03-26 20:43:07 -0700

    Homework comments.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,18 @@
- 
+ // While it may not seem imperative for smaller programs, you should get in the habit
+ // linking to a separate js file and also wrapping your js code in either a 
+
+ // $(document).ready(function(){
+ //  // code goes here
+ // })
+
+ // or an IIFE (immediately invoked function expression)
+ // ;(function(){
+ //  // code goes here
+ // })()
+
+ // One of the most important reasons for that is security - because right now your global variables
+ // can be tampered with through the console by a malicious visitor 😮
+
       var animals = ["dog", "Cat", "Rat", "Bat", "Donkey", "lion", "Tiger", "elephant","Bull","butterfly","deer","Dove","Panda","Fish","Bear"];
 
       // Function for adding buttons
@@ -9,8 +23,8 @@
    
         // Loop through the array of animals, then generate buttons for each animal in the array
         for(i=0; i<animals.length; i++){
-
-            var a = $("<button>");
+          // you should try to keep indentation consistent as it helps with readability
+          var a = $("<button>");
           a.addClass("btn btn-info animalBtn");
         
           a.attr("data-name", animals[i]);
@@ -47,17 +61,21 @@
 
 
         var animal = $(this).attr("data-name");
-        console.log(animal);
+        // you generally want to remove console.log statements from production code
+        // console.log(animal);
         var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + animal + '&api_key=dc6zaTOxFJmzC&limit=10';
 
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
+          // you want to remove any prior gifs from the imageSection before adding any new ones
+          $("#imageSection").empty();
 
          var results = response.data;
 
-
+          // when iterating over the values in an array, feel free to use the native `.forEach` method
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
           for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div class='item'>");
          
@@ -72,8 +90,7 @@
             animalimage.attr("data-state", "still" );
            
             gifDiv.append(animalimage);
-           // gifDiv.append(p);
-             $("#imageSection").prepend(gifDiv);
+            $("#imageSection").prepend(gifDiv);
           }
         });
          
@@ -84,8 +101,8 @@
     
       var state=$(this).attr("data-state");
 
-      console.log(state);
-     
+      // console.log(state);
+      // nice job using strict equality checking 👌
       if (state==="still"){
       
         $(this).attr("src",$(this).attr("data-animate"));
